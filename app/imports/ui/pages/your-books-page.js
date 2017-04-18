092c12fc360afd3143400467c5d1b0a6a63811b3
Mark Cummins <MarkCummins@Marks-MacBook-Air-4.local>
2017-04-18 03:34:38 -1000

    First Commit to Issue 14
    Not done yet

--- app/imports/ui/pages/your-books-page.js
+++ app/imports/ui/pages/your-books-page.js
@@ -1,7 +1,19 @@
-/**
- * Created by MarkCummins o
- * n 4/11/17.
- */
+import { Template } from 'meteor/templating';
+import { YourBookdata } from '../../api/yourbooks/yourbooks.js';
+
+Template.Your_Books_Page.helpers({
+
+  /**
+   * @returns {*} All of the Bookdata documents.
+   */
+  yourbookdataList() {
+    return YourBookdata.find();
+  },
+});
+
+Template.Your_Books_Page.onCreated(function onCreated() {
+  this.subscribe('YourBookdata');
+});
 Template.Sell_Books_Page.onRendered(function enableDropDown() {
   this.$('.dropdown').dropdown();
 });
