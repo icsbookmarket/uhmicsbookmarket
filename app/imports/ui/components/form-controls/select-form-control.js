d7b4169693de78e8741c46eaa9f5eb88b80b1840
Mark Cummins <MarkCummins@Marks-MacBook-Air-4.local>
2017-04-21 13:17:55 -1000

    Sell books dropdown menu works

--- app/imports/ui/components/form-controls/select-form-control.js
+++ app/imports/ui/components/form-controls/select-form-control.js
@@ -3,3 +3,9 @@ import { Template } from 'meteor/templating';
 Template.Select_Form_Control.onRendered(function onRendered() {
   this.$('select.dropdown').dropdown();
 });
+
+Template.Select_Form_Control.helpers({
+  check(options) {
+    console.log(options);
+},
+});
