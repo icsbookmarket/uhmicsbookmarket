cc0ea06e11549d594f39acbd5e387bfeb6360625
shaziney <shaziney@hawaii.edu>
2017-04-25 02:11:43 -1000

    UHM CAS system initial commit.

--- app/client/main.js
+++ app/client/main.js
@@ -1,7 +1,7 @@
 import '/imports/startup/client';
-import '/imports/startup/both';
 import '/imports/api/stuff';
 import '/imports/ui/layouts';
 import '/imports/ui/components/form-controls';
+import '/imports/ui/components/landing';
 import '/imports/ui/pages';
 import '/imports/ui/stylesheets/style.css';
